44b48db4444f9c0af8df9d5b5f2714be9eab5d45
Kevin <ikevin1992@gmail.com>
2021-03-28 17:46:16 -0500

    first commit

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -19,6 +19,11 @@ let puntosJugador = 0,
 
 // referencias html
 const btnPedir = document.querySelector( '#btnPedir' );
+const btnDetener = document.querySelector( '#btnDetener' );
+const btnNuevo = document.querySelector( '#btnNuevo' );
+
+const divCartasJugador = document.querySelector( '#jugador-cartas' );
+const divCartasComputadora = document.querySelector( '#computadora-cartas' );
 const puntosHTML = document.querySelectorAll( 'small' );
 
 
@@ -38,7 +43,7 @@ const crearDeck = () => {
     }
 
     // console.log( deck );
-    deck = _.shuffle( deck );
+    deck = _.shuffle( deck ); // se pone los elementos en forma aleatoria
     console.log( deck );
 
     return deck;
@@ -78,7 +83,54 @@ const valorCarta = ( carta ) => {
 };
 
 
-const valor = valorCarta( pedirCarta() );
+// const valor = valorCarta( pedirCarta() );
+
+// turno de la computadora
+const turnoComputadora = ( puntosMinimos ) => {
+
+    // se usa do while porque se debe ejecutar por lo menos 1 vez
+    do {
+        const carta = pedirCarta();
+
+        puntosComputadora += valorCarta( carta );
+
+        // console.log( { carta, puntosComputadora } );
+        // actualizamos el puntaje en el DOM en la etiqueta small
+        puntosHTML[ 1 ].innerText = puntosComputadora;
+
+        // agregamos la carta con sus clases
+        // <img class="carta" src="assets/cartas/10C.png" alt="">
+        const imgCarta = document.createElement( 'img' );
+        imgCarta.src = `assets/cartas/${ carta }.png`;
+        imgCarta.classList.add( 'carta' );
+        divCartasComputadora.append( imgCarta );
+
+
+        // condicion si el jugador tiene puntaje superior a 21
+        if ( puntosMinimos > 21 ) {
+            break;
+        }
+
+
+    } while ( ( puntosComputadora < puntosMinimos ) && ( puntosMinimos <= 21 ) );
+
+    setTimeout( () => {
+
+        if ( puntosComputadora === puntosMinimos ) {
+            alert( 'Nadie gana :(' );
+        } else if ( puntosMinimos > 21 ) {
+            alert( 'computadora gana' );
+        } else if ( puntosComputadora > 21 ) {
+            alert( 'Jugador gana' );
+        } else if ( puntosComputadora > puntosMinimos ) {
+            alert( 'computadora gana' );
+        }
+
+    }, 10 );
+
+};
+
+
 
 
 // eventos
@@ -91,8 +143,61 @@ btnPedir.addEventListener( 'click', () => {
     // console.log( { carta, puntosJugador } );
     // actualizamos el puntaje en el DOM en la etiqueta small
     puntosHTML[ 0 ].innerText = puntosJugador;
-    
+
+    // agregamos la carta con sus clases
+    // <img class="carta" src="assets/cartas/10C.png" alt="">
+    const imgCarta = document.createElement( 'img' );
+    imgCarta.src = `assets/cartas/${ carta }.png`;
+    imgCarta.classList.add( 'carta' );
+    divCartasJugador.append( imgCarta );
+
+
+    // validacion de puntaje
+    if ( puntosJugador > 21 ) {
+        console.warn( 'Lo siento mucho perdiste' );
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora( puntosJugador );
+
+
+    } else if ( puntosJugador === 21 ) {
+        console.warn( '21 genial' );
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora( puntosJugador );
+    }
 
 } );
 
 
+
+// funcion para btn detener
+btnDetener.addEventListener( 'click', () => {
+
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+
+    turnoComputadora( puntosJugador );
+} );
+
+
+// funcion de nuevo juego
+btnNuevo.addEventListener( 'click', () => {
+
+    console.clear();
+    deck = [];
+    deck = crearDeck();
+
+    puntosComputadora = 0;
+    puntosJugador = 0;
+
+    puntosHTML[ 0 ].innerText = 0;
+    puntosHTML[ 1 ].innerText = 0;
+
+    divCartasJugador.innerHTML = '';
+    divCartasComputadora.innerHTML = '';
+
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+
+} );
